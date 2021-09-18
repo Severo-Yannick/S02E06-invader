765a0de249f2155507a07b4d784b2a7ad0ff3f92
Yannick Severo <severoyannick@gmail.com>
2021-09-18 21:13:39 +0200

    Étape 1 - Générer l'ardoise

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-// ton code ici 
+// Creation de la grille
+function createGrid(dimension) {
+  var invaderDivElement = document.querySelector("#invader");
+
+  for (var i = 0; i < dimension; i++) {
+    var newLineElement = createLine(dimension);
+    invaderDivElement.appendChild(newLineElement);
+  }
+}
+
+// Creation des lignes
+function createLine(dimension) {
+  var lineElement = document.createElement("div");
+  lineElement.classList.add("line");
+
+  for (var i = 0; i < dimension; i++) {
+    var newPixelElement = createPixel();
+    lineElement.appendChild(newPixelElement);
+  }
+
+  return lineElement;
+}
+
+// Création de pixels
+function createPixel() {
+  var pixelElement = document.createElement("div");
+  pixelElement.classList.add("pixel");
+
+  return pixelElement;
+}
+
+createGrid(8);
